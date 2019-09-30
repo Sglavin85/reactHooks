af2dce434dbbb11246c98e97dea94c552e19eabc
Sean Glavin <sean@glavin.us>
2019-09-30 10:27:58 -0500

    useMemo and useDebugValue

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { useRef, createContext } from 'react';
+import React, { useRef, createContext, useMemo } from 'react';
 import Toggle from './Toggle';
-import Counter from './Counter'
+// import Counter from './Counter'
 import useTitleInput from './useTitleInput'
 
 export const UserContext = createContext();
@@ -20,7 +20,18 @@ const App = () => {
   const [name, setName] = useTitleInput('');
 
   const ref = useRef();
-  console.log("ref", ref.current)
+  // console.log("ref", ref.current)
+
+  const reverseWord = (word) => {
+    console.log("Fx Called")
+    return word.split('').reverse().join('');
+  };
+
+  //useMemo should be thought of at "Should function run" in this case, if the variable title change the function runs. Therefore on mount the function will run, however un rerenders during typing in the input box the function does not run.
+  // const title = "Sean Glavin"
+  // const titleReversed = useMemo(() => reverseWord(title), [title]);
+
+
 
   return (
     // <UserContext.Provider
@@ -28,9 +39,9 @@ const App = () => {
     //     user: true
     //   }}>
     <div className="main-wrapper" ref={ref}>
-      <h1 onClick={() => ref.current.classList.add("new-fake-class")}>Level Up Dishes</h1>
+      <h1 onClick={() => ref.current.classList.add("new-fake-class")}>Sean Glavin</h1>
       <Toggle />
-      <Counter />
+      {/* <Counter /> */}
       <form onSubmit={(e) => {
         e.preventDefault();
       }}>
