cdff3e16dc02699d799487d2fa23308a3e0442af
Sean Glavin <sean@glavin.us>
2019-09-30 11:55:19 -0500

    custom body lock scroll and custom on click outside hooks

--- src/hooks/bodyScrollLock.js
+++ src/hooks/bodyScrollLock.js
@@ -2,8 +2,11 @@ import { useLayoutEffect } from 'react';
 
 function useBodyScrollLock() {
     useLayoutEffect(() => {
+        const originalOverflow = window.getComputedStyle(document.body).overflow
         document.body.style.overflow = 'hidden';
-    })
+        //cleanup acts as component will unmount
+        return () => document.body.style.overflow = originalOverflow;
+    }, [])
 }
 
 export { useBodyScrollLock }
